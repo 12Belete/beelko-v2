6fb13ebdb2e8bba8aa709d0f93cd02f6eb25e193
12Belete <154913581+12Belete@users.noreply.github.com>
2026-08-01 10:45:00 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,45 @@
-function login() {
-  const email = document.getElementById("loginEmail").value;
-  const password = document.getElementById("loginPassword").value;
+import { auth } from "./firebase.js";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
-  if (email === "" || password === "") {
-    alert("Please enter your email and password.");
-    return;
-  }
-
-  alert("Login is working!");
-  window.location.href = "home.html";
-}
-
-function signUp() {
+// SIGN UP
+window.signUp = function () {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
-  if (email === "" || password === "") {
-    alert("Please enter your email and password.");
+  if (!email || !password) {
+    alert("Please enter email and password.");
     return;
   }
 
-  alert("Account created successfully!");
-  window.location.href = "login.html";
-}
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      alert("Account created successfully!");
+      window.location.href = "login.html";
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+};
+
+// LOGIN
+window.login = function () {
+  const email = document.getElementById("loginEmail").value;
+  const password = document.getElementById("loginPassword").value;
+
+  if (!email || !password) {
+    alert("Please enter email and password.");
+    return;
+  }
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      alert("Login successful!");
+      window.location.href = "home.html";
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+};
