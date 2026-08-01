16642287e7da07d0a22845cfc22717542a97b50f
12Belete <154913581+12Belete@users.noreply.github.com>
2026-08-01 10:38:03 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,12 +2,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDiM1RFp-yWr9luVLJslNJYiZOhsiwLLO4",
+  authDomain: "beelko-7dcaa.firebaseapp.com",
+  projectId: "beelko-7dcaa",
+  storageBucket: "beelko-7dcaa.firebasestorage.app",
+  messagingSenderId: "757679525639",
+  appId: "1:757679525639:web:faceb6756f2ab687de7fa2"
+};
 };
 
 const app = initializeApp(firebaseConfig);
